036bc33f393dd065f8835d3aa00c74171ba334be
Molchanov Dmitriy <Saigelly@gmail.com>
2024-11-22 12:55:03 +0300

    update code on reducers

--- SaigellyNet/src/Redux/dialoguesReducer.js
+++ SaigellyNet/src/Redux/dialoguesReducer.js
@@ -42,12 +42,13 @@ const dialoguesReducer = (state = initialState, action) => {
                 userId: "1",
                 imgSrc: "https://avatars.mds.yandex.net/i?id=ffc20310cc4dc43bc0db7e94582ee01d5757e375-4884516-images-thumbs&n=13",
             }
-            state.messages.push(newMessage);
-            state.newMessageText = "";
-            return state;
+            return {
+                ...state,
+                messages: [...state.messages, newMessage],
+                newMessageText: "",
+            }
         case UPDATE_NEW_MESSAGE_TEXT:
-            state.newMessageText = action.newText;
-            return state;
+            return { ...state, newMessageText: action.newText }
         default:
             return state;
     }
